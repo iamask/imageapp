1972b1c5d70c0442be5210edd959d84af7420155
Ajay <iamask@live.com>
2023-06-29 11:19:23 +0530

    Update App.js
    updated home  - 2

--- src/App.js
+++ src/App.js
@@ -6,16 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          reactjs.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://pages.cloudflare.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Built on Pages
         </a>
       </header>
     </div>
