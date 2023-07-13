3595d7404713cd446eaca8f49974a9a26a9306e1
Ajay <iamask@live.com>
2023-07-13 15:22:06 +0530

    added multiple function to test module.exports

---
+++ functions/cal.js
@@ -0,0 +1,13 @@
+function sum(x, y) {
+    return (x + y);
+}
+  
+function sub(x, y) {
+    return (x - y);
+}
+  
+function mul(x, y) {
+    return (x * y);
+}
+  
+module.exports = { add, sub, mul, div };
---
+++ functions/main.js
@@ -0,0 +1,11 @@
+const cal = require("./cal.js")  
+  
+//Using the functions from cal.js 
+const sum = cal.sum(2, 2);
+console.log(sum); 
+  
+const sub = cal.sub(10, 5);
+console.log(sub); 
+  
+const product = cal.mul(2, 3);
+console.log(product);
